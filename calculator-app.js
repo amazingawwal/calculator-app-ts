1b95736755dbbb6f578a7405e3fd13aa07afc162
amazingawwal <sakeebmawwal@gmail.com>
2025-07-05 22:07:39 +0100

    Done up till step 5

--- calculator-app.js
+++ calculator-app.js
@@ -1,3 +1,13 @@
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    historySize: 10,
+    history: ['first', 'second']
+})
+
+
 /**
  * STEP 2
  * This is a Calculator App that does basic arithmetic operations:
@@ -61,8 +71,14 @@ class Calculator{
      * @returns number
      */
     divide(firstNumber, secondNumber){
-        if(secondNumber === 0) throw new Error("You can't divide by 0. Enter a number other than 0");
-        return firstNumber / secondNumber;
+        try {
+            if(secondNumber === 0) throw new Error(`You can't divide by 0. Enter a number other than 0`);
+            else{
+                return firstNumber / secondNumber;
+            }
+        } catch (error) {
+            return `${error.message}`
+        }
         // this.result = firstNumber / secondNumber;
         // return this.result;
     };
@@ -77,20 +93,19 @@ class Calculator{
 
 
 /**
- * STEP 3
+ * STEP 3, 4 qnd 5
  * Command Line Interface
  * shows a 'Welcome Message'
  * shows 'Available Options'
  * Main Menu Loop Implementation
  */
 
-const readline = require('node:readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
+/**
+ * This menu class houses 2 methods:
+ * - greet method that logs the initial greeting
+ * - display option method that shows different available arithmetic operations
+ */
 class Menu{
     constructor(){
 
@@ -116,7 +131,10 @@ class Menu{
 
 /**
  * This class Operations help perform arithmetic operations.
- * It has 2 dependencies (dependent classes), Menu and Calculator
+ * It has 2 dependencies (dependent classes), Menu and Calculator.
+ * It has 2 methods:
+ * - rlQuestion method which servers as a utility for serving different arithmetic operation
+ * - userInput method which handles user input that determines choice of operations
  */
 class Operations{
     constructor(calculate, menu){
@@ -125,27 +143,16 @@ class Operations{
     }
 
     /**
-     * This is a Readline(Question) reusable function to help keep the code DRY
+     * This is a Readline(Question) reusable method to help keep the code DRY
      * @param {function} operation 
      */
-    // rlQuestion(operation){
-    //     rl.question('Enter first number:', (num1)=>{
-
-    //         rl.question('Enter second number:', (num2)=>{
-    //             const number1 = parseInt(num1)
-    //             const number2 = parseInt(num2)
-    //             console.log(operation(number1, number2))
-    //             rl.close()
-    //         })
-    //     })
-    // }
-
     rlQuestion(operation){
+        //recursive function for input validation and to allow user retry after invalid input
         function recursiveQuestion (){
             rl.question('Enter first number:', (num1)=>{
                 rl.question('Enter second number:', (num2)=>{
-                    const number1 = parseInt(num1)
-                    const number2 = parseInt(num2)
+                    const number1 = parseFloat(num1)
+                    const number2 = parseFloat(num2)
                     if (isNaN(number1) || isNaN(number2)) {
                         console.log(`${number1} or ${number2} is not a number. Enter valid numbers only `)
                         recursiveQuestion();
@@ -158,6 +165,40 @@ class Operations{
         }
         return recursiveQuestion();
     }
+
+
+
+    /**
+     * This is User Input method helps determine the operations type to be performed
+     */
+    userInput(){
+        this.menu.greet();
+        this.menu.displayOptions()
+        rl.on('line', (input)=>{
+            if(parseInt(input) === 1){
+                this.rlQuestion(this.calculate.add)
+            }else if (parseInt(input) === 2){
+                this.rlQuestion(this.calculate.subtract)
+            }else if (parseInt(input) === 3){
+                this.rlQuestion(this.calculate.multiply)
+            }else if (parseInt(input) === 4){
+                this.rlQuestion(this.calculate.divide)
+            }else if (parseInt(input) === 5){
+                rl.close();
+            }else{
+                console.log(`Enter a valid input from 1-5`)
+            }
+        })
+    }
+
+};
+
+console.log(rl.history);
+const menu = new Menu();
+const calculate = new Calculator();
+const performOperation = new Operations(calculate, menu);
+performOperation.userInput();
+
 
     // addition(){
     //     rl.question('Enter first number:', (num1)=>{
@@ -181,32 +222,3 @@ class Operations{
     //         })
     //     })
     // }
-
-    userInput(){
-        this.menu.greet();
-        this.menu.displayOptions()
-        rl.on('line', (input)=>{
-            if(parseInt(input) === 1){
-                this.rlQuestion(this.calculate.add)
-            }else if (parseInt(input) === 2){
-                this.rlQuestion(this.calculate.subtract)
-            }else if (parseInt(input) === 3){
-                this.rlQuestion(this.calculate.multiply)
-            }else if (parseInt(input) === 4){
-                this.rlQuestion(this.calculate.divide)
-            }else if (parseInt(input) === 5){
-                rl.close();
-            }else{
-                console.log(`Enter a valid input from 1-5`)
-            }
-        })
-    }
-
-};
-
-const menu = new Menu();
-const calculate = new Calculator();
-const performOperation = new Operations(calculate, menu);
-performOperation.userInput();
-
-
