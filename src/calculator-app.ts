717ee016f04465311b6084f6dc811f1a0be7b270
amazingawwal <sakeebmawwal@gmail.com>
2025-07-19 20:53:31 +0100

    step 8 done

--- src/calculator-app.ts
+++ src/calculator-app.ts
@@ -1,14 +1,28 @@
 import * as readline from 'node:readline/promises';
 
-// interface CalculationHistory {
-//     operation: string;
-//     operand1: number;
-//     operand2: number;
-//     result: number;
-// }
+enum OperationChoice {
+    ADD = 1,
+    SUBTRACT = 2,
+    MULTIPLY = 3,
+    DIVIDE = 4,
+    EXIT = 5
+}
 
-// let historyStore: CalculationHistory[] = [];
-let historyStore: number[] = [];
+interface CalculatorOperation {
+    name: string;
+    symbol: string;
+    operation: (a: number, b: number) => number;
+}
+
+interface CalculationHistory {
+    operation: string;
+    operand1: number;
+    operand2: number;
+    result: number;
+}
+
+let historyStore: CalculationHistory[] = [];
+
 
 const rl: readline.Interface = readline.createInterface({
     input: process.stdin,
@@ -26,27 +40,54 @@ type Operations = {
 //     return a + b;
 // }
 
-const add: Operations = (a:number, b:number): number => a+b;
+const add: CalculatorOperation = {
+    name: 'Addition',
+    symbol: '+',
+    operation: (a:number, b:number): number => a+b
+}
 
-function subtract(a: number, b: number): number {
-    return a - b;
-};
+const subtract: CalculatorOperation = {
+    name: 'Subtraction',
+    symbol: '-',
+    operation: (a:number, b:number): number => a-b
+}
 
-function multiply(a: number, b: number): number {
-    return a * b;
-};
+const multiply: CalculatorOperation = {
+    name: 'Multiplication',
+    symbol: '*',
+    operation: (a:number, b:number): number => a*b
+}
+// function multiply(a: number, b: number): number {
+//     return a * b;
+// };
 
-function divide(a: number, b: number): number {
-    try {
-        if(b === 0) throw new Error(`You can't divide by 0. Enter a number other than 0`);
-        else{
-            return a / b;
+
+const divide: CalculatorOperation = {
+    name: 'Division',
+    symbol: '/',
+    operation: (a:number, b:number): number => {
+        try {
+            if(b === 0) throw new Error(`You can't divide by 0. Enter a number other than 0`);
+            else{
+                return a / b;
             }
         } 
-    catch (error: any) {
+        catch (error: any) {
             return error.message
         }
+    }
 };
+// function divide(a: number, b: number): number {
+//     try {
+//         if(b === 0) throw new Error(`You can't divide by 0. Enter a number other than 0`);
+//         else{
+//             return a / b;
+//             }
+//         } 
+//     catch (error: any) {
+//             return error.message
+//         }
+// };
 
 
 // const result = divide(2,0);
@@ -103,7 +144,7 @@ function isValidNumber(value: string): boolean {
 }
 
 
-async function operations(calculate: Operations) {
+async function operations(calculate: Operations): Promise<OperationChoice> {
 
     const number1 = await rl.question('Enter your first number:');
     const number2 = await rl.question('Enter your second number:');
@@ -111,50 +152,51 @@ async function operations(calculate: Operations) {
     if(isValidNumber(number1) || isValidNumber(number2)){
         console.error(`${number1} and ${number2} are not valid numbers. Enter valid numbers`);
         rl.close;
-    }{
+    }
         const firstNumber = await getNumberInput(number1);
         const secondNumber = await getNumberInput(number2);
         const result = calculate(firstNumber, secondNumber)
-        historyStore.push(result)
-        console.log(result);
+        console.log(`Result is: ${result}`);
         rl.close();
-    }
-
+    
+    return result
 }
 
 // function exitGracefully(): void {
 //     rl.close
 // };
 
-async function calculate(): Promise<void> {
+async function calculate(): Promise<OperationChoice> {
 
     mainMenuLoop();
 
     const prompt  = await promptHandler();
     const promptNumber = await getNumberInput(prompt);
     switch (promptNumber) {
-        case 1:
-            operations(add)
-            break;
-        case 2:
-            operations(subtract)
-            break;
-        case 3:
-            operations(multiply)
-            break;
-        case 4:
-            operations(divide)
-            break;
+        case OperationChoice.ADD:
+            console.log(`You chose ${add.name} - '${add.symbol}'`)
+            return operations(add.operation)
+            // break;
+        case OperationChoice.SUBTRACT:
+            console.log(`You chose ${subtract.name} - '${subtract.symbol}'`)
+            return operations(subtract.operation)
+            // break;
+        case OperationChoice.MULTIPLY:
+            console.log(`You chose ${multiply.name} - '${multiply.symbol}'`)
+            return operations(multiply.operation)
+            // break;
+        case OperationChoice.DIVIDE:
+            console.log(`You chose ${divide.name} - '${divide.symbol}'`)
+            return operations(divide.operation)
+            // break;
         case 5:
+            console.log(`BYE..`)
             rl.close()
-            break;
+            // break;
         default:
             console.log('Enter a number from 1-5 for your preferred operation');
-            calculate();
+            return calculate();
     }
 }
 
 calculate();
-
-
-
